43dfd16bf8ed592181a4542f4627dff3536690be
mehrshadsamaei <mehrshadsamaei0074@gmail.com>
2022-05-31 04:02:24 -0700

    commit and push add exception application arraylist

---
+++ src/ApplicationArrayList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ApplicationArrayList {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        System.out.println("what is size your list?");
+        try {
+            int size = in.nextInt();
+            try {
+                for (int i = 0; i < size; i++) {
+                    System.out.println("enter num");
+                    int num = in.nextInt();
+                    arrayList.add(num);
+                }
+            System.out.println(arrayList);
+            ArrayListMethode.arrayList(arrayList);
+            }catch (Exception e){
+                System.out.println("enter correct number");
+            }
+        }catch (Exception e){
+            System.out.println("enter correct number");
+        }
+    }
+}
